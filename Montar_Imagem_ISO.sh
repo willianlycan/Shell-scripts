bd5b0cae02f97682362485546c0188778c57c88e
willianlycan <59544098+willianlycan@users.noreply.github.com>
2020-01-21 19:16:53 -0400

    Update Montar_Imagem_ISO.sh

--- Montar_Imagem_ISO.sh
+++ Montar_Imagem_ISO.sh
@@ -8,20 +8,21 @@ function ISO() {
   echo -e -n "\n => O nome da pasta é" "\033[5m$PASTA""\033[0m ? [S/N] "
    read NOME_PASTA
     case $NOME_PASTA in
-    s|S|"") mkdir /mnt/$PASTA ;;
+    s|S|"") [ -d /mnt/$PASTA ] && echo -e "\n\033[46m /mnt/$PASTA""\033[0m""\033[5m já existe!""\033[0m" || mkdir /mnt/$PASTA ;;
     n|N|"") ISO ;;
     *) echo -e "\n\033[41mOpção inválida!""\033[0m\n" ; exit ;;
  esac
-   
- echo -e "\n 2º) Montar a imagem iso \n"
- echo " => Digite o caminho completo para o diretório" 
- echo -n "     em que o arquivo .iso está: "
-  read DIR_ISO
- echo -e -n "\n => Digite o nome completo do arquivo .iso: "
-  read NOME_ISO
- echo -e -n "\n => A iso" "\033[5m$NOME_ISO""\033[0m encontra-se em" "\033[46m$DIR_ISO""\033[0m ? [S/N] "
-  read MONTAR
-   case $MONTAR in
+ 
+ sleep 3
+  echo -e "\n 2º) Montar a imagem iso \n"
+  echo " => Digite o caminho completo para o diretório" 
+  echo -n "     em que o arquivo .iso está: "
+   read DIR_ISO
+  echo -e -n "\n => Digite o nome completo do arquivo .iso: "
+   read NOME_ISO
+  echo -e -n "\n => A iso" "\033[5m$NOME_ISO""\033[0m encontra-se em" "\033[46m$DIR_ISO""\033[0m ? [S/N] "
+   read MONTAR
+    case $MONTAR in
     s|S|"") mount -o loop $DIR_ISO/$NOME_ISO /mnt/$PASTA ; echo -e "\n Processo concluído!\n" ;;
     n|N|"") rm -rf /mnt/$PASTA ; echo -e "\n Processo abortado!\n" ;;
     *) echo -e "\n\033[41mOpção inválida!""\033[0m\n" ; rm -rf /mnt/$PASTA ;;
@@ -30,10 +31,9 @@ function ISO() {
  echo -n "Sair? [S/N] "
   read SAIR
    case $SAIR in
-    s|S|"") exit ;;
-    n|N|"") ISO ;;
-    *) echo -e "\n\033[41mOpção inválida!""\033[0m\n" ; exit ;;
+   s|S|"") exit ;;
+   n|N|"") ISO ;;
+   *) echo -e "\n\033[41mOpção inválida!""\033[0m\n" ; exit ;;
  esac 
 }
-
 ISO
