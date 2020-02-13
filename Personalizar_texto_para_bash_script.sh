089bcb5ae93baa75537ace1cbcb696479240332f
willianlycan <59544098+willianlycan@users.noreply.github.com>
2020-02-13 08:33:11 -0400

    Add files via upload

---
+++ Personalizar_texto_para_bash_script.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+ clear && echo -e '''
+  \033[0m  Padrão  \033[0m|\033[3m  Itálico  \033[0m|  \033[4mSublinhada\033[0m
+  
+  \033[1m  Efeito Bold  \033[0m|\033[5m  Efeito Blink \033[0m
+  
+  \033[34m  Azul  \033[0m|  \033[44m Padrão com faixa azul \033[0m
+  
+  \033[33m  Laranja  \033[0m|  \033[43m Padrão com faixa laranja \033[0m
+  
+  \033[35m  Roxo  \033[0m|  \033[45m Padrão com faixa roxa \033[0m
+  
+  \033[36m  Verde água  \033[0m|  \033[46m Padrão com faixa verde água \033[0m
+  
+  \033[32m  Verde claro  \033[0m|  \033[42m Padrão com faixa verde claro \033[0m
+  
+  \033[31m  Vermelho  \033[0m|  \033[41m Padrão com faixa vermelha \033[0m
+  \033[0m''' && exit
